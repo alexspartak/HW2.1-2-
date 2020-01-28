ca69ce4903da2939a1c6a905ca7c9366f319874d
Alex Tishchenko <a.tishencko2017@yandex.ru>
2020-01-29 01:15:41 +0300

    Initial Commit

---
+++ HW2.1(2)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW2.1(2)
+//
+//  Created by Alex Tishchenko on 29.01.2020.
+//  Copyright © 2020 Alex Tishchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
